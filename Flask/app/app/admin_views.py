1278e7db7074c2c826bc4e886c637a89fb5a8b64
shubhamwankar <shubhamwankar23@gmail.com>
2023-03-24 03:30:10 +0530

    updated model and files in template format

--- Flask/app/app/admin_views.py
+++
@@ -1,9 +0,0 @@
-from app import app
-
-@app.route('/admin/dashboard')
-def admin_dashboard():
-    return 'Admin dashborad'
-
-@app.route('/admin/profile')
-def admin_profile():
-    return 'Admin profile'
